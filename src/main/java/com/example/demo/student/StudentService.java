257653796fcd17baa07ee253ec632e4c56b15aa7
Supantha Paul <supapaul56@gmail.com>
2023-01-14 17:10:07 +0530

    first commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+	public List<Student> getStudents() {
+		return List.of(
+			new Student(1L, "Supantha", "supantha@gmail.com", LocalDate.of(2001, Month.JULY, 2), 21)
+		);
+	}
+}
